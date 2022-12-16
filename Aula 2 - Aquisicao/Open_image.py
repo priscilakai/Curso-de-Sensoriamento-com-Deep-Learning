44c2bdd4b13f179493305fdaa3a9dcf0b3b91abc
Priscila M Kai <43282604+priscilakai@users.noreply.github.com>
2022-12-16 15:06:48 -0300

    Create Open_image.py

---
+++ Aula 2 - Aquisicao/Open_image.py
@@ -0,0 +1,29 @@
+# Abrindo uma imagem .TIF com Spyder
+
+# Importação de Bibliotecas/Pacotes
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import numpy as np
+import rasterio
+from os import listdir
+from os.path import isfile, join
+from rasterio.enums import Resampling
+from rasterio.mask import mask
+
+# Lendo KML files com GeoPandas
+gpd.io.file.fiona.drvsupport.supported_drivers['KML'] = 'rw'
+my_map = gpd.read_file('F:/Doutorado/Curso.kml', driver='KML')
+
+# Convertendo EPSG para o mesmo da imagem do Sentinel-2
+my_map = my_map.to_crs(epsg=32722)
+    
+# Pasta contendo images
+path_input = 'F:/Doutorado/Curso/'
+
+# Obtendo nomes de arquivos de imagem
+img_nomes = [f for f in listdir(path_input) if isfile(join(path_input, f))]
+full_path = join(path_input,img_nomes[3])
+
+# Carregando dados de imagem em uma variável
+arq = rasterio.open(full_path)
+Banda4 = arq.read()
